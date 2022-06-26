07181ea66e40c9e91116eb392fd6dcc1ec9b54e5
Ezike, Omera <Omera_Ezike@comcast.com>
2022-06-26 19:34:42 -0400

    first commit

---
+++ web/public/javascripts/films.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('#filmsTable').hide();
+});
+
+var API = (() => {
+    var filmArray = [];
+    var createFilm = () => {
+
+        var formData = new FormData(document.forms.filmSubmit);
+        var filmName = formData.get('filmName');
+
+        if (filmName.length > 0){
+            filmArray.push(filmName);
+            console.log("Film added: " + filmName);
+            alert("Film added: " + filmName);
+            $("#filmName").val("");
+        }
+        return false;
+    }
+
+    var getFilms = () => {
+
+        if (filmArray.length > 0) {
+            var filmRating = 10;
+            $('#filmsTable').show();
+            $("#filmsTable").find("tr:gt(0)").remove();
+            console.log("Films:" + filmArray.toString());
+            $.each(filmArray, function (index, filmName) {
+                $("#filmsTable").append("<tr><td>" + filmName + "</td><td>" + filmRating + "</td></tr>");
+            });
+
+        }
+        return false;
+    }
+
+    return {
+        createFilm,
+        getFilms
+    }
+})();
